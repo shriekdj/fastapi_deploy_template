e0c1c2197644f1a8e802c77a0f2de14a1f6fffef
Shrikant Dhayaje <shrikantdhayaje@gmail.com>
2025-10-22 13:46:21 +0530

    updated for using uvicorn in main.py at root folder

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from fastapi-deploy-template!")
+import uvicorn
 
+PROJECT_ENVIRONMENT = "development"
 
 if __name__ == "__main__":
-    main()
+    # print(settings.environment)
+    if PROJECT_ENVIRONMENT == "production":
+        uvicorn.run("app.main:app", host="127.0.0.1", port=8000, workers=4)
+    else:
+        uvicorn.run(
+            "app.main:app",
+            host="127.0.0.1",
+            port=8000,
+            log_level="info",
+            reload=True,
+        )
